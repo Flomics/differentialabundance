20c919a559b48a54695020199d7fa896f6741525
ctuni <c.tuni.dominguez@gmail.com>
2023-06-16 17:00:12 +0200

    update

--- bin/edger_de.R
+++ bin/edger_de.R
@@ -12,37 +12,37 @@ counts <- args[4]
 count_matrix <- read.table(counts, header = TRUE, sep = ",", row.names = 1)
 
 metadata <- read.table(samplesheet, header = TRUE, sep = ",")
-count_matrix <- count_matrix[, names(count_matrix) %in% metadata$sample]
-metadata <- metadata[metadata$sample %in% names(count_matrix), ]
+# count_matrix <- count_matrix[, names(count_matrix) %in% metadata$sample]
+# metadata <- metadata[metadata$sample %in% names(count_matrix), ]
 
 head(count_matrix)
 head(metadata)
 
-group <- metadata$condition
-group <- factor(group)
+# group <- metadata$condition
+# group <- factor(group)
 
-y <- DGEList(
-    counts = count_matrix,
-    genes = rownames(count_matrix),
-    group = metadata$condition)
+# y <- DGEList(
+#     counts = count_matrix,
+#     genes = rownames(count_matrix),
+#     group = metadata$condition)
 
-keep <- filterByExpr(y, y$samples$group)
-table(keep)
-y <- y[keep, , keep.lib.sizes = FALSE]
+# keep <- filterByExpr(y, y$samples$group)
+# table(keep)
+# y <- y[keep, , keep.lib.sizes = FALSE]
 
-y <- calcNormFactors(y)
+# y <- calcNormFactors(y)
 
-design <- model.matrix(~0 + y$samples$group)
-colnames(design) <- levels(y$samples$group)
-design
+# design <- model.matrix(~0 + y$samples$group)
+# colnames(design) <- levels(y$samples$group)
+# design
 
-y <- estimateDisp(y, design, robust = TRUE)
+# y <- estimateDisp(y, design, robust = TRUE)
 
-fit <- glmQLFit(y, design, robust = TRUE)
+# fit <- glmQLFit(y, design, robust = TRUE)
 
-contrast_comp <- makeContrasts(target - reference, levels = design)
-res <- glmQLFTest(fit, contrast = contrast_comp)
-topTags(res)
+# contrast_comp <- makeContrasts(target - reference, levels = design)
+# res <- glmQLFTest(fit, contrast = contrast_comp)
+# topTags(res)
 
 
 # ################################################
